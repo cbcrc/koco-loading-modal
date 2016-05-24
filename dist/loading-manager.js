df8075da37dc8ac3da8768432733f91b254d0ba3
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 14:45:25 -0400

    update to koco2

---
+++ dist/loading-manager.js
@@ -0,0 +1,63 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _modaler = require('modaler');
+
+var _modaler2 = _interopRequireDefault(_modaler);
+
+var _knockout = require('knockout');
+
+var _knockout2 = _interopRequireDefault(_knockout);
+
+var _jquery = require('jquery');
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var LoadingManager = function LoadingManager() {
+    var self = this;
+    _modaler2.default.registerModal('loading', {
+        basePath: 'bower_components/koco-loading-modal/src',
+        backdrop: 'static',
+        keyboard: false
+    });
+
+    self.message = _knockout2.default.observable('');
+}; // Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+LoadingManager.prototype.show = function (message) {
+    var self = this;
+
+    return _jquery2.default.Deferred(function (dfd) {
+        if (self.message()) {
+            self.message(message);
+        } else {
+            self.message(message);
+
+            _modaler2.default.show('loading', {
+                message: self.message,
+                disabelKeyEvents: true
+            }, function () {
+                dfd.resolve();
+            });
+        }
+    }).promise();
+};
+
+LoadingManager.prototype.hide = function () {
+    var self = this;
+
+    return _jquery2.default.Deferred(function (dfd) {
+        _modaler2.default.hideCurrentModal().then(function () {
+            self.message('');
+            dfd.resolve();
+        });
+    }).promise();
+};
+
+exports.default = new LoadingManager();
---
+++ dist/loading-modal-ui.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+var ViewModel = function ViewModel(context /*, componentInfo*/) {
+    this.message = context.params.message;
+};
+
+exports.default = ViewModel;
